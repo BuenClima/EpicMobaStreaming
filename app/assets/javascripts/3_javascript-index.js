c753cb8e2910653280f2e7a2d2bf38cc2f87bc6e
alejandro <alejandro@alsnet.es>
2018-01-14 13:20:41 +0000

    model, controllers, views, devise, ajax added

--- app/assets/javascripts/3_javascript-index.js
+++ app/assets/javascripts/3_javascript-index.js
@@ -6,21 +6,24 @@ function loadFirstStreamers() {
     $.getJSON("http://localhost:3000/api/getStreamers",function (dataJson) {
         var row = $(document.createElement('div'));
         row.addClass('row');
-        console.log(dataJson[0].data);
         $.each(dataJson[0].data, function (index,element) {
             if(index >= 3){
                 return;
+            }
+            var url = "/streamer/index";
+            if (element.streamer_type === 1) {
+                url = "/tournament/index";
             }
             var column = $(document.createElement('div'));
             column.addClass('col-sm-4');
             column.append("<div class='streamer'>" +
                             "<div class='streamName'>" +
-                                "<a class='link' href='" + element.url + "' target='_self'><p class='lead'>" + element.title + "</p></a>" +
+                                "<a class='link' href='" + url + "' target='_self'><p class='lead'>" + element.twitch_username + "</p></a>" +
                             "</div>" +
                             "<div class='miniature'>" +
-                                "<a class='link' href='" + element.url + "' target='_self'><img class='img-fluid' alt='miniatura' src='" + element.thumbnail_url + "'></a>" +
+                                "<a class='link' href='" + url + "' target='_self'><img class='img-fluid' alt='miniatura' src='" + element.image + "'></a>" +
                             "</div>" +
-                            "<p class='stream-info'><strong>" + element.user_name + "</strong> con <strong>" + element.viewer_count + "</strong> espectadores</p>" +
+                            "<p class='stream-info'><strong>" + element.twitch_username + "</strong> con <strong>" + element.twitch_user_id + "</strong> espectadores</p>" +
                            "</div>");
             row.append(column);
         });
@@ -29,27 +32,30 @@ function loadFirstStreamers() {
 }
 
 function loadMoreStreamers() {
-    //alert('Hey');
-    $.getJSON("http://localhost:3000/api/getStreamers",function (dataJson) {
-        var min = document.getElementById("streamerCursor");
+    var min = document.getElementById("streamerCursor");
+    $.getJSON("http://localhost:3000/api/getStreamersBy/" + min.value,function (dataJson) {
         var count = 0;
         var row = $(document.createElement('div'));
         row.addClass('row');
-        $.each(dataJson.data, function (index, element) {
+        $.each(dataJson, function (index, element) {
             if (element.id > min.value) {
                 if (count >= 3) {
                     return;
+                }
+                var url = "/streamer/index";
+                if (element.streamer_type === 1) {
+                    url = "/tournament/index";
                 }
                 var column = $(document.createElement('div'));
                 column.addClass('col-sm-4');
                 column.append("<div class='streamer'>" +
                                 "<div class='streamName'>" +
-                                    "<a class='link' href='" + element.url + "' target='_self'><p class='lead'>" + element.title + "</p></a>" +
+                                    "<a class='link' href='" + url + "' target='_self'><p class='lead'>" + element.twitch_username + "</p></a>" +
                                 "</div>" +
                                 "<div class='miniature'>" +
-                                    "<a class='link' href='" + element.url + "' target='_self'><img class='img-fluid' alt='miniatura' src='" + element.thumbnail_url + "'></a>" +
+                                    "<a class='link' href='" + url + "' target='_self'><img class='img-fluid' alt='miniatura' src='" + element.image + "'></a>" +
                                 "</div>" +
-                                "<p class='stream-info'><strong>" + element.user_name + "</strong> con <strong>" + element.viewer_count + "</strong> espectadores</p>" +
+                                "<p class='stream-info'><strong>" + element.twitch_username + "</strong> con <strong>" + element.twitch_user_id + "</strong> espectadores</p>" +
                                "</div>");
                 row.append(column);
                 count++;
